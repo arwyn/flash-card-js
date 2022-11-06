97c2994469b5e7e8b13f082a535e9e1a80b54a58
Arwyn Hainsworth <arwynh@gmail.com>
2022-11-06 09:24:19 +0000

    Add MUI

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,5 +12,20 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      allowSyntheticDefaultImports: false, // speeds up storybook build time
+      esModuleInterop: false, // speeds up storybook build time
+      shouldExtractLiteralValuesFromEnum: true, // makes union prop types like variant and size appear as select controls
+      shouldRemoveUndefinedFromOptional: true, // makes string and boolean types that can be undefined appear as inputs and switches
+      propFilter: (prop) =>
+        prop.parent
+          ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName)
+          : true,
+    },
   }
 }
