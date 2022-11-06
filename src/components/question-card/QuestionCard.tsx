805ad0aadf6433fe23af241814b522979b738e71
Arwyn Hainsworth <arwynh@gmail.com>
2022-11-06 09:24:21 +0000

    Added functionality to card component

--- src/components/question-card/QuestionCard.tsx
+++ src/components/question-card/QuestionCard.tsx
@@ -1,19 +1,60 @@
 import React from 'react';
 import { Card, CardProps, CardContent, CardActions, Button, ButtonProps, Grid } from '@mui/material';
-import { LooksOne, LooksTwo, Looks3, Looks4 } from '@mui/icons-material';
+import { LooksOne, LooksTwo, Looks3, Looks4, ThumbUp, ThumbDown } from '@mui/icons-material';
 import { Markdown } from '../markdown/Markdown';
 import { randomizeList, shuffle } from '../../util/random.uitl';
+import { useState } from 'react';
 
-interface QuestionCardProps {
+export type ButtonAction = (button: any, answerId?: string) => void;
+
+export type AnswerAction = (questionId?: string, nswerId?: string) => void;
+
+/**
+ * The Question Card Properties
+ */
+export interface QuestionCardProps {
+    /** Question ID */
+    id?: string;
+    /** Question (Block Markdown) */
     question: string;
+    /** Correct Answer (Inline Markdown) */
     correctAnswer: string;
+    /**
+     * Possible Incorrect Answers (Inline Markdown)
+     * 
+     * Upto 3 elements will be chosen.
+     */
     incorrectAnswers: string[];
+    /** MUI Card properties */
     card?: CardProps,
-    button?: ButtonProps
+    /** MUI Button Properties */
+    button?: ButtonProps;
+    /**
+     * Event Triggered for correct answer
+     */
+    onCorrectAnswer?: AnswerAction;
+    /**
+     * Event Triggered for incorrect answer
+     */
+    onWrongAnswer?: AnswerAction;
+    /**
+     *  Icon for correctly selected entry
+     *  @default `<ThumbsUp/>`
+    */
+    correctIcon?: React.ReactNode;
+    /**
+     *  Icon for incorrectly selected entry
+     *  @default `<ThumbsDown/>`
+    */
+    wrongIcon?: React.ReactNode;
 }
 
 /**
- * Display Markdown Text as HTML
+ * Question Card
+ * 
+ * Will display a question and upto 4 possible answer buttons, only 1 of which is correct.
+ * 
+ * When a button is pressed, the button will change the button icon, the button color, disable the button and call the corresponding event.
  */
 export const QuestionCard = ({
     question,
@@ -21,14 +62,30 @@ export const QuestionCard = ({
     incorrectAnswers = [],
     card = { variant: 'outlined' },
     button = {variant: 'contained', fullWidth: true},
+    onCorrectAnswer = () => {},
+    onWrongAnswer = () => {},
+    correctIcon = <ThumbUp/>,
+    wrongIcon = <ThumbDown/>,
     ...props
 }: QuestionCardProps) => {
+    const [answers,] = useState(shuffle([
+            { correct: true, text: correctAnswer}
+        ].concat(randomizeList(incorrectAnswers.map(text => ({correct: false, text})), 3)))
+        .map((answer, i) => ({...answer, id: `answer-${i}`})));
 
-    const answers = shuffle([
-        { correct: true, text: correctAnswer }
-    ].concat(randomizeList(incorrectAnswers.map(text => ({correct: false, text})), 3)));
+    const [disabled, setDisabled] = useState<string[]>([]);
 
     const icons = [(<LooksOne/>), (<LooksTwo/>), (<Looks3/>), (<Looks4/>)];
+
+    const action = (button: any, answerId: string) => {
+        setDisabled((prev) => prev.concat([answerId]));
+        if (answers.find(({id}) => id === answerId)?.correct as boolean){
+            onCorrectAnswer(props.id, answerId);
+        }
+        else {
+            onWrongAnswer(props.id, answerId)
+        }
+    };    
 
     return (
         <Card {...card}>
@@ -37,11 +94,23 @@ export const QuestionCard = ({
             </CardContent>
             <CardActions>
                 <Grid container spacing={2}>
-                    {answers.map(({text}, i) => (
-                        <Grid item xs={6}>
-                            <Button {...button} startIcon={icons[i]}><Markdown inline text={text}/></Button>
-                        </Grid>
-                    ))}
+                    {answers.map(({text, correct, id}, i) => {
+                        const off = disabled.find(item => item === id) !== undefined;
+                        const icon = off ? (correct ? correctIcon : wrongIcon) : icons[i];
+                        const color = off ? (correct ? 'success' : 'error') : 'primary';
+                        return (
+                            <Grid item xs={6} key={i}>
+                                <Button 
+                                    {...button}
+                                    disabled={off}
+                                    startIcon={icon}
+                                    color={color}
+                                    onClick={(button) => action(button, id)}>
+                                        <Markdown inline text={text}/>
+                                </Button>
+                            </Grid>
+                        );
+                    })}
                 </Grid>
             </CardActions>
         </Card>);
